0d72d11beffa536a1fd6c22c2e02c04ec420c386
nickscip <nickscip@gmail.com>
2025-02-27 15:24:53 -0600

    feat: brewfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,13 +7,17 @@ if test ! "$(which omz)"; then
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 fi
 
-# Check fo rHomebrew and install it if we don't have it
-# if test ! "$(which brew)"; then
-#   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-#
-#   echo "eval '$(/opt/homebrew/bin/brew shellenv)'" >> "$HOME/.zprofile"
-#   eval "$(/opt/homebrew/bin/brew shellenv)"
-# fi
-
 # Symlinks the .zshrc to the .dotfiles
 ln -sw "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
+
+# Check for Homebrew and install it if we don't have it
+if test ! "$(which brew)"; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle --file ./Brewfile
