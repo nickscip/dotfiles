d714f8a906a3581aaff48332b475a1874f17ecfc
nickscip <nickscip@gmail.com>
2025-02-27 14:23:38 -0600

    wip: bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Setting up your Mac"
+
+# Check for Oh My Zsh and install it if we don't have it
+if test ! "$(which omz)"; then
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+fi
+
+# Check fo rHomebrew and install it if we don't have it
+# if test ! "$(which brew)"; then
+#   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#
+#   echo "eval '$(/opt/homebrew/bin/brew shellenv)'" >> "$HOME/.zprofile"
+#   eval "$(/opt/homebrew/bin/brew shellenv)"
+# fi
